c4e63aadba846bffd111b0748e60781d99584fe4
kamran2100805 <kamran@ovikfinance.com>
2023-02-23 13:06:13 +0530

    cors addd

--- server.js
+++ server.js
@@ -40,20 +40,23 @@ app.use(cors({
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT,PATCH')
-//   res.header("Access-Control-Allow-Headers",
-//     "Origin, Content-Type, Accept, Authorization, Special-Request-Header, X-Requested-With");
-//   if (req.method === "OPTIONS") {
-//     req.header('Access-Control-Allow-Methods', 'PUT,POST,GET,PATCH,DELETE,')
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT,PATCH')
+  res.header("Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept, Authorization, Special-Request-Header, X-Requested-With");
+  if (req.method === "OPTIONS") {
+    req.header('Access-Control-Allow-Methods', 'PUT,POST,GET,PATCH,DELETE,')
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
 
 
 var allowCrossDomain = function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8000', 'http://localhost:4200');
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
